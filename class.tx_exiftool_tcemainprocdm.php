80632c7f7990e8c018a0f8f90a9d40b14b60f886
maholtz <maholtz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-26 14:31:18 +0000

    writes categories into file meta-data now
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/exiftool/trunk@14285 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_exiftool_tcemainprocdm.php
+++ class.tx_exiftool_tcemainprocdm.php
@@ -30,10 +30,13 @@ class tx_exiftool_tcemainprocdm
 	{
 		global $FILEMOUNTS, $BE_USER, $TYPO3_CONF_VARS;
 // TODO: take care of UTF-8/latin1 conversion if needed - exiftool param: -L
+
 		if ($table == 'tx_dam') {
 			if ($status == 'update') {
+
+// t3lib_div::debug($table.' '.$status.' '.$id,'Tabelle und Status und ID');
+
 				$config = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['exiftool']);
-t3lib_div::debug($config);
 				if (1 != $config['writeMetaData']) {
 					// TODO: Debug-Message
 					return 0;
@@ -43,27 +46,21 @@ t3lib_div::debug($config);
 					return 0;
 				}
 
-				// t3lib_div::debug(dirname(dirname($GLOBALS['_SERVER']['SCRIPT_FILENAME'])));
 				// TODO: check if this assumtion is right - especially for fe-editing!
 				$absolutePath = dirname(dirname($GLOBALS['_SERVER']['SCRIPT_FILENAME']));
 
-				// t3lib_div::debug($id);
-				// Änderungen: t3lib_div::debug($fieldArray);
 				$file = $absolutePath.'/'.$reference->checkValue_currentRecord['file_path'].$reference->checkValue_currentRecord['file_name'];
-// t3lib_div::debug($file);
-				// checkValue_currentRecord
-				// t3lib_div::debug($reference);
-				// [datamap][tx_dam][$id]
+
 				$this->info['exec'] = 'perl';
 
-				// TODO: read from configuration
 				$page_id = $config['mediapid'];
 				$this->service_conf = t3lib_BEfunc::getModTSconfig($page_id,'tx_exiftool_sv1');
-// t3lib_div::debug($this->service_conf);
+
 				$this->info['params'] = ' '.t3lib_extMgm::extPath('exiftool').'exiftool/exiftool ';
 				$match = $this->service_conf['properties']['match.'];
 
 				$params = array(); // Array for all params
+
 				// Example:
 				// keywords.tag.1 = -keywords
         		// keywords.tag.1.splitToken = ,
@@ -72,17 +69,17 @@ t3lib_div::debug($config);
 						continue;
 					}
 					$fieldName = mb_substr($field,0,-1); // strip of the last dot 'keywords.'
-					if (!isset($fieldArray[$fieldName])) {
+					if (!isset($fieldArray[$fieldName]) && !isset($tagConfiguration['tag.']['ignoreUnchangedValue'])) {
 						// we get only the changed values, so we should only write the changed values
 						continue;
 					}
 					$i = 1;
 					while (isset($tagConfiguration['tag.'][(string)$i])) {
 						$tmpparams = ' '.$tagConfiguration['tag.'][(string)$i].'=\''.$fieldArray[$fieldName].'\' ';
+						// splitToken is used
 						if (isset($tagConfiguration['tag.'][(string)$i.'.']['splitToken'])) {
 							// special handling
 							$list = t3lib_div::trimExplode($tagConfiguration['tag.'][(string)$i.'.']['splitToken'], $fieldArray[$fieldName]);
-// t3lib_div::debug($list);
 							if (!is_array($list)) {
 								// empty list? should be written too
 								// we can use the default configuration for that
@@ -94,44 +91,38 @@ t3lib_div::debug($config);
 								$tmpparams .= ' '.$tagConfiguration['tag.'][(string)$i].'=\''.$listitem.'\' ';
 							}
 						}
+						// lookUpCategory  is used
+						if (isset($tagConfiguration['tag.'][(string)$i.'.']['lookUpCategory'])) {
+
+							$res = $GLOBALS['TYPO3_DB']->exec_SELECT_mm_query('tx_dam_cat.title',
+									'tx_dam',
+									'tx_dam_mm_cat',
+									'tx_dam_cat',
+									' AND tx_dam_mm_cat.uid_local = '.(int)$id,
+									'',
+									'',
+									''
+								);
+							$tmpparams = '';
+							while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res))	{
+								$tmpparams .= ' '.$tagConfiguration['tag.'][(string)$i].'=\''.$row['title'].'\' ';
+							}
+						}
+
 						$i++;
 						$params[] = $tmpparams;
 					}
 				}
 
 				$this->info['params'] .= ' '.implode(' ',$params).' ';
-				// $this->info['params'] .= ' -keywords=\''.addslashes($fieldArray['keywords']).'\'';
 				$cmd = t3lib_exec::getCommand($this->info['exec']).$this->info['params'].' '.$file.'';
 				$output = array();
 				$ret = -1;
 				exec($cmd, $output, $ret);
-				// t3lib_div::debug($cmd);
+// t3lib_div::debug($cmd);
 
 			}
 		}
-		/*
-
-  [historyRecords] => Array
-        (
-            [tx_dam:3] => Array
-                (
-                    [oldRecord] => Array
-                        (
-                            [keywords] => test93
-                        )
-
-                    [newRecord] => Array
-                        (
-                            [keywords] => test94
-                        )
-
-                )
-
-        )
-
-
-		 */
-
 
 	}
 
